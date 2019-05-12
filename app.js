abf19f85f10a11ece482e3b1f618fb3c71feafaa
Thanche <thanche91@gmail.com>
2019-05-12 11:31:23 +0300

    some progress

---
+++ app.js
@@ -0,0 +1,37 @@
+//Init Github
+const github = new Github;
+
+//Init UI class
+
+const ui = new UI;
+
+
+
+//Search input 
+
+const searchUser = document.getElementById('searchUser');
+
+//Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+
+
+    const userText = e.target.value;
+
+    if (userText !== '') {
+        //Make http call 
+        github.getUser(userText)
+            .then(data => {
+                if (data.profile.message === 'Not Found') {
+                    //Show alert
+                } else {
+                    //Show the profile
+                    ui.showProfile( data.profile );
+                }
+            })
+    } else {
+        //Clear profile
+
+    }
+
+
+});
